02682fb5ae3c6a482973c4b4231ad1acd8a77698
fuerte <leandrofuerte75@gmail.com>
2025-01-16 10:52:44 +0100

    Eliminating unnecessary html

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const bgchange = document.querySelector(".background-change");
 
+for (let i = 0; i < 15; i++) {
+  bgchange.innerHTML += `<p>Scroll down</p>`;
+}
+
 bgchange.addEventListener("scroll", () => {
   const scrollPercentage =
     bgchange.scrollTop / (bgchange.scrollHeight - bgchange.clientHeight);
   bgchange.style.backgroundColor = `hsl(${scrollPercentage * 50}, 100%, 50%)`;
 });
-
