2c5f55b8148661a2242a28eaa5b6973693034168
fuerte <leandrofuerte75@gmail.com>
2025-01-16 10:41:08 +0100

    Fixing errors

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const bgchange = document.querySelector(".background-change");
-      bgchange.addEventListener("scroll", () => {
-        const scrollPercentage =
-          bgchange.scrollTop / (bgchange.sqcrollHeight - bgchange.clientHeight);
-        bgchange.style.backgroundColor = `hsl(${
-          scrollPercentage * 50
-        }, 100%, 50%)`;
-      });
+
+bgchange.addEventListener("scroll", () => {
+  const scrollPercentage =
+    bgchange.scrollTop / (bgchange.scrollHeight - bgchange.clientHeight);
+  bgchange.style.backgroundColor = `hsl(${scrollPercentage * 50}, 100%, 50%)`;
+});
+
